a7376ff3b5f05e4bd99382d15390c48053aaec0c
David Green <coding@davidrg.com>
2017-11-18 23:24:47 -0600

    Wrap the googletag.pubads().refresh function and begin storing data from it

--- page/collect-data.js
+++ page/collect-data.js
@@ -4,10 +4,36 @@ var DFPeep = ( function() {
 	window.googletag = window.googletag || {};
 	window.googletag.cmd = window.googletag.cmd || [];
 
-	var slots = {};
+	var slots = {},
+		refreshHistory = [];
 
 	var init = function() {
 		console.log( 'init' );
+		wrapGPTFunctions();
+	};
+
+	var wrapGPTFunctions = function() {
+		googletag.cmd.push(
+			function() {
+				wrapGPTRefresh();
+		} );
+	};
+
+	var wrapGPTRefresh = function() {
+		var oldVersion = googletag.pubads().refresh;
+		googletag.pubads().refresh = function() {
+			var refreshData = {
+				slotIds: []
+			};
+			var refreshed = arguments[0];
+			for ( var i = 0, length = refreshed.length; i < length; i++ ) {
+				refreshData.slotIds.push( refreshed[ i ].getSlotElementId() );
+			}
+			refreshHistory.push( refreshData );
+			console.log( refreshHistory );
+			var result = oldVersion.apply( this, arguments );
+			return result;
+		};
 	};
 
 	return {
