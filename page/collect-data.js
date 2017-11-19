ada020b86a88d060b604bafc051d3126460854d6
David Green <coding@davidrg.com>
2017-11-18 23:06:50 -0600

    Create the global googletag.cmd array to hold functions that need to wait to execute until GPT is loaded

--- page/collect-data.js
+++ page/collect-data.js
@@ -1,6 +1,9 @@
 var DFPeep = ( function() {
 	'use strict';
 
+	window.googletag = window.googletag || {};
+	window.googletag.cmd = window.googletag.cmd || [];
+
 	var slots = {};
 
 	var init = function() {
